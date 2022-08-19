91d0ce55ea32e3689ed44afad98cfc316be11322
ELHARFALI <elharfali.youssef.20@gmail.com>
2022-08-19 17:55:00 +0100

    Nav

--- main.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.getElementById("menu-btn");
-const navbar = document.querySelector(".navbar");
-
-const menuButton = function () {
-    menuBtn.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
-};
-
-menuBtn.addEventListener("click", menuButton);
